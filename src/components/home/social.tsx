b64c50b6355e95a27bac1a43824742b2a3458d63
Frederick Go <dn282145@dal.ca>
2023-02-07 09:44:25 -0400

    Social bug fix

--- src/components/home/social.tsx
+++ src/components/home/social.tsx
@@ -4,13 +4,13 @@ function Social() {
     return (
         <div className="main-social">
             <a className="contact-social" href="https://www.linkedin.com/in/fred-go/" target="_blank" rel="noreferrer">
-            <Image alt="Frederick's LinkedIN" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
+            <img alt="Frederick's LinkedIN" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/linkedin.svg" />
             </a>
             <a className="contact-social" href="https://github.com/itsmefredgo" target="_blank" rel="noreferrer">
-                <Image alt="Frederick's GitHub" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/github.svg" />
+                <img alt="Frederick's GitHub" src="https://raw.githubusercontent.com/peterthehan/peterthehan/master/assets/github.svg" />
             </a>
             <a className="contact-social" href="https://www.instagram.com/itsmefredgo/" target="_blank" rel="noreferrer">
-                <Image alt="Frederick's Instagram" src="https://raw.githubusercontent.com/hussainweb/hussainweb/main/icons/instagram.png" />
+                <img alt="Frederick's Instagram" src="https://raw.githubusercontent.com/hussainweb/hussainweb/main/icons/instagram.png" />
             </a>
         </div>   
     )  
